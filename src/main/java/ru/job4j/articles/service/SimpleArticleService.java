ff0c2aa5d266a4ce516cb2b431c832778742b2a0
smarkov2021 <markovsv777@mail.ru>
2023-10-30 17:56:38 +0300

    b

--- src/main/java/ru/job4j/articles/service/SimpleArticleService.java
+++ src/main/java/ru/job4j/articles/service/SimpleArticleService.java
@@ -24,7 +24,7 @@ public class SimpleArticleService implements ArticleService {
     public void generate(Store<Word> wordStore, int count, Store<Article> articleStore) {
         LOGGER.info("Геренация статей в количестве {}", count);
         var words = wordStore.findAll();
-        var articles = IntStream.iterate(0, i -> i < count, i -> i + 1)
+        var articles = IntStream.iterate(0, i -> i < count - 1, i -> i + 1)
                 .peek(i -> LOGGER.info("Сгенерирована статья № {}", i))
                 .mapToObj((x) -> articleGenerator.generate(words))
                 .collect(Collectors.toList());
